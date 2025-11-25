2c5dee4bc6abeab8b2cc82fb76c74ab2b2375afc
Antonio <antoniomilosevic306@gmail.com>
2025-11-25 21:58:28 +0100

    values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -5,3 +5,8 @@ console.log(40 + 8 + 23 - 10);
 console.log("Jonas");
 
 let firstName = "Jonas";
+console.log(firstName);
+let PI = 3.145;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
