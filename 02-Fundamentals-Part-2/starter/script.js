03b7905e3346c10f3bf58bd339fbae64f4da8cd4
Antonio <antoniomilosevic306@gmail.com>
2025-12-27 19:36:29 +0100

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,7 +1,26 @@
 "use strict";
-
+/*
 let hasDriversLicence = false;
 const passTest = true;
 
 if (passTest) hasDriversLicence = true;
 if (hasDriversLicence) console.log(`I can drive`);
+*/
+
+function logger() {
+  console.log("My name is Toni");
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
