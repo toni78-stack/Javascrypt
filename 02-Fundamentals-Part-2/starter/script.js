859d2ecf2a2b0554472b363d1c96baa5f60b1cb8
Antonio <antoniomilosevic306@gmail.com>
2026-01-03 21:10:06 +0100

    objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -188,6 +188,7 @@ if (friends.includes("Steven")) {
   console.log("I have a friend called Steven");
 }
 */
+/*
 
 const calcTip = function (bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
@@ -200,4 +201,13 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 console.log(bills, tips);
 
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
-console.log(bills, tips, totals);
+console.log(bills, tips, totals);  */
+
+const Jonas = {
+  firstName: "Jonas",
+  lastName: "Schmdt",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Jon"],
+};
+console.log(Jonas);
