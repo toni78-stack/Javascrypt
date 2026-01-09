9ddfda450c4d52a69b0e183c4f544a3d87e385c2
Antonio <antoniomilosevic306@gmail.com>
2026-01-09 17:41:39 +0100

    exerc

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict"; /*
 /*
 let hasDriversLicence = false;
 const passTest = true;
@@ -124,7 +124,7 @@ console.log(friends);
 
 const y = new Array(1991, 1994, 1996, 1969);
 
-console.log(friends[0]);
+console.lg(friends[0]);
 console.log(friends[2]);
 console.log(friends.length);
 
@@ -267,8 +267,8 @@ const Jonas = {
 
 console.log(Jonas.calcAge());
 console.log(Jonas.getSummary());
-*/ /*
-
+*/
+/*
 const mark = {
   fullName: "Mark Miller",
   mass: 78,
@@ -378,7 +378,7 @@ for (let exercise = 1; exercise < 4; exercise++) {
     console.log(`Exercise ${exercise}: Lifting weights repetition ${rep}`);
   }
 } */
-
+/*
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`Lifting weights repetition ${rep}`);
 }
@@ -403,10 +403,40 @@ for (let exercise = 1; exercise < 4; exercise++) {
   for (let rep = 1; rep < 6; rep++) {
     console.log(`Exercise ${exercise}: Lifting weights repetition ${rep}`);
   }
-
-  for (let set = 1; rep < 3; set++) {
-    console.log(
-      `Biceps curl ${set}: Lifting weights repetition per set ${set}`
-    );
-  }
 }
+*/
+//Create an array called bills containing all 10 test bill values.
+//Create empty arrays for the tips and the totals (tips and totals)
+//Use the calcTip function we wrote before (included in the starter code)
+//to calculate tips and total values (bill + tip) for every bill value in the bills array.
+//Use a for loop to perform the 10 calculations!
+//TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86, and 52
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    //sum = sum + arr[i];
+    sum += arr[i];
+  }
+
+  return sum / arr.length;
+};
+
+console.log(calcAverage([2, 3, 6]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
