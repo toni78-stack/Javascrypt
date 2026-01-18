41c33c0f224155ac72cc6afabfb94f0fd1b198b5
Antonio <antoniomilosevic306@gmail.com>
2026-01-18 19:28:19 +0100

    arrow

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -555,3 +555,14 @@ console.log(calcAverage(3, 4, 5));
 const scoreDolphins = calcAverage(44, 23, 71);
 const scoreKoalas = calcAverage(65, 54, 49);
 console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win the throphy(${avgDolphins}vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win the trophy (${avgKoalas}vs. ${avgDolphins})`);
+  } else {
+    console.log(`No one wins...`);
+  }
+};
+checkWinner(scoreDolphins, scoreKoalas)
