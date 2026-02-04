9336576d6f51feac2466a4b4e7ed4a236c567960
Antonio <antoniomilosevic306@gmail.com>
2026-02-04 20:03:52 +0100

    working

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -23,9 +23,13 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct number';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too high!';
-    score--; //score=score-1
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--; //score=score-1
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'Too low';
     score--;
